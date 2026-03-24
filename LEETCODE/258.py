604ba436f8850ae237b22bc0718c8c9ad5e916ed
nideeshnagaraj-cmyk <nideeshnagaraj@gmail.com>
2026-03-24 23:30:05 +0530

    Delete LEETCODE directory

--- LEETCODE/258.py
+++
@@ -1,5 +0,0 @@
-num='38'
-while len(num)!=1:
-    nums=list(num)
-    print(sum(map(int,nums)))
-    num=str(nums)
--- LEETCODE/7.py
+++
@@ -1,6 +0,0 @@
-x=-123
-sign=-1 if x<0 else 1
-x=abs(x)
-y=str(x)[::-1]
-r=int(y) if (-2**31<=int(y)<=2**31-1) else 0
-print(r*sign)
